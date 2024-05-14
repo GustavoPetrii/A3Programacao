13ae8d1a17784cfe2c5e3fc275bc20acd3053391
1072410916 <1072410916@UNS11DT099206.animaedu.intranet>
2024-05-14 20:21:54 -0300

    Implementando as classes principais

--- src/model/CadastroAmigo.java
+++ src/model/CadastroAmigo.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author 1072413039
- */
 public class CadastroAmigo {
-    
+
+    private String nome;
+    private String telefone;
+
+
+    public CadastroAmigo() {
+        this("", "");
+    }
+
+    public CadastroAmigo(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+   
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- src/model/CadastroFerramenta.java
+++ src/model/CadastroFerramenta.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author 1072413039
- */
 public class CadastroFerramenta {
+
+    private String nome;
+    private String marca;
+    private double custo;
+    private int quantidade;
+
+    public CadastroFerramenta() {
+        this("", "", 0, 0);
+    }
+
+    public CadastroFerramenta(String nome, String marca, double custo, int quantidade) {
+        this.nome = nome;
+        this.marca = marca;
+        this.custo = custo;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCusto() {
+        return custo;
+    }
+
+    public void setCusto(double custo) {
+        this.custo = custo;
+    }
     
+    public int getQuantidade() {
+        return quantidade;
+    }
+    
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 }
+
